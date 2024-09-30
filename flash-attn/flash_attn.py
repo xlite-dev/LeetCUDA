2868f21c2d99c4da4d38c4974eac1f5195a254a8
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-09-30 09:49:54 +0800

    Update flash_attn.py

--- flash-attn/flash_attn.py
+++ flash-attn/flash_attn.py
@@ -35,7 +35,7 @@ def naive_attn(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
 def run_benchmark(perf_func: callable, 
                   q: torch.Tensor, k: torch.Tensor, v: torch.Tensor,
                   tag: str, out: Optional[torch.Tensor] = None, 
-                  warmup: int = 10, iters: int = 200,
+                  warmup: int = 2, iters: int = 20,
                   show_all: bool = False):
     if out is not None: 
         out.fill_(0)      
@@ -62,33 +62,43 @@ def run_benchmark(perf_func: callable,
     out_info = f"out_{tag}"
     out_val = out.flatten().detach().cpu().numpy().tolist()[:3]
     out_val = [round(v, 8) for v in out_val]
-    print(f"{out_info:>17}: {out_val}, time:{mean_time:.8f}ms")
+    out_val = [f"{v:<12}" for v in out_val]
+    print(f"{out_info:>24}: {out_val}, time:{mean_time:.6f}ms")
     if show_all: print(out[0, 0, 0, :])
     return out.clone(), mean_time
 
-
-print("-" * 80)
+Bs = [4, 8]
+Hs = [8]
+Ns = [256, 512, 1024]
+Ds = [64, 128] # only support [64, 128]
 # batch_size, n_head, seq_len, head_dim (B,nh,N,d)
-B, nh, N, d = 16, 12, 64, 64
-q = torch.randn(B, nh, N, d).float().cuda().contiguous()
-k = torch.randn(B, nh, N, d).float().cuda().contiguous()
-v = torch.randn(B, nh, N, d).float().cuda().contiguous()
-o = torch.randn(B, nh, N, d).float().cuda().contiguous()
-q.requires_grad = False
-k.requires_grad = False
-v.requires_grad = False
-o.requires_grad = False
-run_benchmark(lib.flash_attn_1_fwd_f32,    q, k, v, "fa1f32")
-run_benchmark(lib.flash_attn_1_fwd_f32_v2, q, k, v, "fa1f32(v2)", o)
-run_benchmark(naive_attn,                  q, k, v, "attnf32_th")
+BHNDs = [(B, H, N, D) for B in Bs for H in Hs for N in Ns for D in Ds]
 
-print("-" * 80)
-# using fp16 Tesor Core mma instruction
-q_f16 = q.half()
-k_f16 = k.half()
-v_f16 = v.half()
-o_f16 = o.half()
-run_benchmark(lib.flash_attn_2_fwd_f16_mma_m16n8k16,    q_f16, k_f16, v_f16, "fa2mmaf16")
-run_benchmark(lib.flash_attn_2_fwd_f16_mma_m16n8k16_v2, q_f16, k_f16, v_f16, "fa2mmaf16(v2)", o_f16)
-run_benchmark(naive_attn,                               q_f16, k_f16, v_f16, "attnf16_th")
-print("-" * 80)
+print("-" * 100)
+print(" "* 25 + "B: batch_size, H: n_head, N: seq_len, D: head_dim")
+for (B, H, N, D) in BHNDs:
+    print("-" * 100)
+    print(" " * 40 + f"B={B}, H={H}, N={N}, D={D}")
+    q = torch.randn(B, H, N, D).float().cuda().contiguous()
+    k = torch.randn(B, H, N, D).float().cuda().contiguous()
+    v = torch.randn(B, H, N, D).float().cuda().contiguous()
+    o = torch.randn(B, H, N, D).float().cuda().contiguous()
+    if D <= 64:
+        run_benchmark(lib.flash_attn_1_fwd_f32,    q, k, v, "FA1f32")
+        run_benchmark(lib.flash_attn_1_fwd_f32_v2, q, k, v, "FA1f32(nocopy)", o)
+        run_benchmark(naive_attn,                  q, k, v, "f32_th")
+    
+    if D in (64, 128):
+        print("-" * 100)
+        # using fp16 Tesor Core MMA instruction
+        q_f16 = q.half()
+        k_f16 = k.half()
+        v_f16 = v.half()
+        o_f16 = o.half()
+        run_benchmark(lib.flash_attn_2_fwd_f16_mma_m16n8k16,    
+                      q_f16, k_f16, v_f16, "FA2MMAf16")
+        run_benchmark(lib.flash_attn_2_fwd_f16_mma_m16n8k16_v2, 
+                      q_f16, k_f16, v_f16, "FA2MMAf16(nocopy)", o_f16)
+        run_benchmark(naive_attn,                               
+                      q_f16, k_f16, v_f16, "f16_th")
+    print("-" * 100)
