57205482f3302987e2a965b91c6f93d670c6cd4d
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-10-07 19:02:44 +0800

    Update hgemm.py

--- hgemm/hgemm.py
+++ hgemm/hgemm.py
@@ -98,8 +98,8 @@ for (M, N, K) in MNKs:
                   a, b, "f16x8pack(k16+dbuf+offset)",        c)
     run_benchmark(lib.hgemm_t_8x8_sliced_k16_f16x8_pack_dbuf_async,              
                   a, b, "f16x8pack(k16+dbuf+async)",         c)
-    run_benchmark(lib.hgemm_t_8x8_sliced_k16_f16x8_pack_dbuf_async_offset,       
-                  a, b, "f16x8pack(k16+dbuf+async+offset)",  c)
+    run_benchmark(lib.hgemm_t_8x8_sliced_k32_f16x8_pack_dbuf,              
+                  a, b, "f16x8pack(k32+dbuf)",               c)
     run_benchmark(partial(torch.matmul, out=c),                            
                   a, b, "f16_th")
     print("-" * 110)
