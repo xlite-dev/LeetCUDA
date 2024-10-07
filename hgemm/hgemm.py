dd9de3000955d222635d741cb00121bc63356ab5
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-10-07 21:44:30 +0800

    Update hgemm.py

--- hgemm/hgemm.py
+++ hgemm/hgemm.py
@@ -92,7 +92,7 @@ for (M, N, K) in MNKs:
                   a, b, "f16x8pack(bcf+offset)",             c)
     run_benchmark(lib.hgemm_t_8x8_sliced_k_f16x8_pack_bcf_dbuf,            
                   a, b, "f16x8pack(bcf+dbuf)",               c)
-    print("-" * 50 + "  Async   " + "-" * 50)
+    print("-" * 50 + "--Async---" + "-" * 50)
     run_benchmark(lib.hgemm_t_8x8_sliced_k16_f16x8_pack_dbuf,              
                   a, b, "f16x8pack(k16+dbuf)",               c)
     run_benchmark(lib.hgemm_t_8x8_sliced_k16_f16x8_pack_dbuf_offset,       
