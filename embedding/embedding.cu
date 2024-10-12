07e81fec7a148755ba38796aadf5c7a1508d89cf
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-10-12 09:40:18 +0800

    Update embedding.cu

--- embedding/embedding.cu
+++ embedding/embedding.cu
@@ -12,68 +12,69 @@
 
 #define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
 #define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
+
 __global__ void embedding_f32_kernel(const int *idx, float *weight, float *output, int n, int emb_size)
 {
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    int tid = bx * blockDim.x + tx;
-    int offset = idx[bx] * emb_size;
-    output[bx * emb_size + tx] = weight[offset + tx];
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int tid = bx * blockDim.x + tx;
+  int offset = idx[bx] * emb_size;
+  output[bx * emb_size + tx] = weight[offset + tx];
 }
 
 __global__ void embedding_f32x4_kernel(const int *idx, float *weight, float *output, int n, int emb_size)
 {
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    int tid = bx * blockDim.x + tx;
-    int offset = idx[bx] * emb_size;
-    output[bx * emb_size + tx] = weight[offset + tx];
-    output[bx * emb_size + tx + 1] = weight[offset + tx + 1];
-    output[bx * emb_size + tx + 2] = weight[offset + tx + 2];
-    output[bx * emb_size + tx + 3] = weight[offset + tx + 3];
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int tid = bx * blockDim.x + tx;
+  int offset = idx[bx] * emb_size;
+  output[bx * emb_size + tx] = weight[offset + tx];
+  output[bx * emb_size + tx + 1] = weight[offset + tx + 1];
+  output[bx * emb_size + tx + 2] = weight[offset + tx + 2];
+  output[bx * emb_size + tx + 3] = weight[offset + tx + 3];
 }
 
 __global__ void embedding_f32x4_pack_kernel(const int *idx, float *weight, float *output, int n, int emb_size)
 {
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    int tid = bx * blockDim.x + tx;
-    int offset = idx[bx] * emb_size;
-    LDST128BITS(output[bx * emb_size + 4 * tx]) = LDST128BITS(weight[offset + 4 * tx]);
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int tid = bx * blockDim.x + tx;
+  int offset = idx[bx] * emb_size;
+  LDST128BITS(output[bx * emb_size + 4 * tx]) = LDST128BITS(weight[offset + 4 * tx]);
 }
 
 __global__ void embedding_f16_kernel(const int *idx, half *weight, half *output, int n, int emb_size)
 {
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    int tid = bx * blockDim.x + tx;
-    int offset = idx[bx] * emb_size;
-    output[bx * emb_size + tx] = weight[offset + tx];
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int tid = bx * blockDim.x + tx;
+  int offset = idx[bx] * emb_size;
+  output[bx * emb_size + tx] = weight[offset + tx];
 }
 
 __global__ void embedding_f16x8_kernel(const int *idx, half *weight, half *output, int n, int emb_size)
 {
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    int tid = bx * blockDim.x + tx;
-    int offset = idx[bx] * emb_size;
-    output[bx * emb_size + tx] = weight[offset + tx];
-    output[bx * emb_size + tx + 1] = weight[offset + tx + 1];
-    output[bx * emb_size + tx + 2] = weight[offset + tx + 2];
-    output[bx * emb_size + tx + 3] = weight[offset + tx + 3];
-    output[bx * emb_size + tx + 4] = weight[offset + tx + 4];
-    output[bx * emb_size + tx + 5] = weight[offset + tx + 5];
-    output[bx * emb_size + tx + 6] = weight[offset + tx + 6];
-    output[bx * emb_size + tx + 7] = weight[offset + tx + 7];
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int tid = bx * blockDim.x + tx;
+  int offset = idx[bx] * emb_size;
+  output[bx * emb_size + tx] = weight[offset + tx];
+  output[bx * emb_size + tx + 1] = weight[offset + tx + 1];
+  output[bx * emb_size + tx + 2] = weight[offset + tx + 2];
+  output[bx * emb_size + tx + 3] = weight[offset + tx + 3];
+  output[bx * emb_size + tx + 4] = weight[offset + tx + 4];
+  output[bx * emb_size + tx + 5] = weight[offset + tx + 5];
+  output[bx * emb_size + tx + 6] = weight[offset + tx + 6];
+  output[bx * emb_size + tx + 7] = weight[offset + tx + 7];
 }
 
 __global__ void embedding_f16x8_pack_kernel(const int *idx, half *weight, half *output, int n, int emb_size)
 {
-    int tx = threadIdx.x;
-    int bx = blockIdx.x;
-    int tid = bx * blockDim.x + tx;
-    int offset = idx[bx] * emb_size;
-    LDST128BITS(output[bx * emb_size + 8 * tx]) = LDST128BITS(weight[offset + 8 * tx]);
+  int tx = threadIdx.x;
+  int bx = blockIdx.x;
+  int tid = bx * blockDim.x + tx;
+  int offset = idx[bx] * emb_size;
+  LDST128BITS(output[bx * emb_size + 8 * tx]) = LDST128BITS(weight[offset + 8 * tx]);
 }
 
 // --------------------- PyTorch bindings for custom kernel -----------------------
