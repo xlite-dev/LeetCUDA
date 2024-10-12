e1542ebdb27da49d3727e14260cfdfa108981365
bear-zd <a827569457@163.com>
2024-10-12 16:11:55 +0800

    [Softmax] Add online softmax f32x4 pack kernel

--- softmax/softmax.py
+++ softmax/softmax.py
@@ -100,6 +100,7 @@ run_benchmark(lib.softmax_f32_per_token,              x, "f32(per)",         out
 run_benchmark(lib.softmax_f32x4_per_token,            x, "f32x4(per)",       out)
 run_benchmark(lib.safe_softmax_f32_per_token,         x, "f32(safe)",        out) 
 run_benchmark(lib.online_safe_softmax_f32_per_token,  x, "f32(safe+online)", out)
+run_benchmark(lib.online_safe_softmax_f32x4_pack_per_token,  x, "f32x4(safe+online)", out)
 run_benchmark(lib.safe_softmax_f32x4_per_token,       x, "f32x4(safe)",      out) 
 run_benchmark(partial(torch.softmax, dim=1, out=out), x, "f32_th(per)")
 
@@ -123,6 +124,7 @@ run_benchmark(lib.softmax_f32_per_token,              x, "f32(per)",         out
 run_benchmark(lib.softmax_f32x4_per_token,            x, "f32x4(per)",       out)
 run_benchmark(lib.safe_softmax_f32_per_token,         x, "f32(safe)",        out) 
 run_benchmark(lib.online_safe_softmax_f32_per_token,  x, "f32(safe+online)", out)
+run_benchmark(lib.online_safe_softmax_f32x4_pack_per_token,  x, "f32x4(safe+online)", out)
 run_benchmark(lib.safe_softmax_f32x4_per_token,       x, "f32x4(safe)",      out) 
 run_benchmark(partial(torch.softmax, dim=1, out=out), x, "f32_th(per)")
 
