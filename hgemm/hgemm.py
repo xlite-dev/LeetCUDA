502b390034e9233e1febaf722e4e04b0efd372fa
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-11-22 11:57:08 +0800

    Update hgemm.py

--- hgemm/hgemm.py
+++ hgemm/hgemm.py
@@ -6,6 +6,11 @@ from torch.utils.cpp_extension import load
 from functools import partial
 from typing import Optional
 import argparse
+from utils import (get_build_sources, 
+                   get_build_cuda_cflags, 
+                   get_device_capability, 
+                   get_device_name, 
+                   row2col)
 
 torch.set_grad_enabled(False)
 
@@ -60,87 +65,6 @@ args = get_args()
 pretty_print_line()
 print(args)
 pretty_print_line()
-
-
-def get_device_name():
-    device_name = torch.cuda.get_device_name(torch.cuda.current_device())
-    # since we will run GPU on WSL2, so add WSL2 tag.
-    if "Laptop" in device_name:
-        device_name += " WSL2"
-    return device_name
-
-
-def get_device_capability():
-    return torch.cuda.get_device_capability(torch.cuda.current_device())
-
-
-def get_build_sources():
-    build_sources = []
-    build_sources.append('naive/hgemm.cu')
-    build_sources.append('naive/hgemm_async.cu')
-    build_sources.append('cublas/hgemm_cublas.cu')
-    build_sources.append('wmma/hgemm_wmma.cu')
-    build_sources.append('wmma/hgemm_wmma_stage.cu')
-    build_sources.append('mma/hgemm_mma.cu')
-    build_sources.append('mma/hgemm_mma_stage.cu')
-    build_sources.append('mma/hgemm_mma_stage_tn.cu')
-    build_sources.append('cutlass/hgemm_mma_stage_tn_cute.cu')
-    build_sources.append('pybind/hgemm.cc')
-    return build_sources
-
-
-def get_project_dir():
-    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-def get_build_cuda_cflags():
-    # -Xptxas -v:
-    # registers, smem, cmem, stack, gmem usage
-    # registers: 寄存器，访问速度最快。Ada Lovelace架构每个SM的寄存器文件大小
-    # 为256KB，这相当于65536个32位寄存器，65536/256=256。一个SM可以同时执行多
-    # 个block，对一个Kernel，同时存在于一个SM中的Block和Warp数量取决于SM中可用
-    # 且所需的寄存器和共享内存数量。每个Thread需要的寄存器越多，那么SM中的Warp就
-    # 越少。即减少Thread所需寄存器数量，即可增加SM中的Warp数。每个Block需要的共
-    # 享内存越多，那么SM中可以被同时处理的Block就会变少。即减少每个Block所需的共
-    # 享内存，即可同时处理更多Block。SM内的资源没办法处理一个完整Block，Kernel
-    # 将无法启动。
-    # cmem: 常量内存，被缓存，访问速度快。
-    # stack frame: 由于寄存器的数量有限，当需要使用的变量数量超过可用寄存器数量时，
-    # 编译器会将某些变量从寄存器“溢出”到栈上，这个过程称为spill。访问栈上的数据比
-    # 访问寄存器慢得多。
-    # spill stores: 指的是在执行过程中，数据因为寄存器不足而被存储到了栈上。
-    # spill loads: 则是指将之前溢出到栈上的数据重新加载回寄存器。
-    # diag 177: variable was declared but never referenced
-    extra_cuda_cflags = []
-    extra_cuda_cflags.append("-O3")
-    extra_cuda_cflags.append("-U__CUDA_NO_HALF_OPERATORS__")
-    extra_cuda_cflags.append("-U__CUDA_NO_HALF_CONVERSIONS__")
-    extra_cuda_cflags.append("-U__CUDA_NO_HALF2_OPERATORS__")
-    extra_cuda_cflags.append("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
-    extra_cuda_cflags.append("--expt-relaxed-constexpr")
-    extra_cuda_cflags.append("--expt-extended-lambda")
-    extra_cuda_cflags.append("--use_fast_math")
-    extra_cuda_cflags.append("-diag-suppress 177")
-    extra_cuda_cflags.append("-Xptxas -v")
-    # extra cuda flags for cute hgemm
-    project_dir = get_project_dir()
-    extra_cuda_cflags.append('-DENBLE_CUTE_HGEMM')
-    extra_cuda_cflags.append('-DNO_MMA_HGEMM_BIN')
-    extra_cuda_cflags.append('-DNO_WMMA_HGEMM_BIN')
-    extra_cuda_cflags.append('-DNO_CUTE_HGEMM_BIN')
-    extra_cuda_cflags.append('-DNO_CUBLAS_HGEMM_BIN')
-    # add cutlass headers and link cublas.
-    extra_cuda_cflags.append(f'-I {project_dir}')
-    extra_cuda_cflags.append(f'-I {project_dir}/utils')
-    extra_cuda_cflags.append(f'-I {project_dir}/naive')
-    extra_cuda_cflags.append(f'-I {project_dir}/wmma')
-    extra_cuda_cflags.append(f'-I {project_dir}/mma')
-    extra_cuda_cflags.append(f'-I {project_dir}/cutlass')
-    extra_cuda_cflags.append(f'-I {project_dir}/pybind')
-    extra_cuda_cflags.append(f'-I {project_dir}/third-party/cutlass/include')
-    extra_cuda_cflags.append(f'-I {project_dir}/third-party/cutlass/tools/util/include')
-    extra_cuda_cflags.append('-lcublas')
-    return extra_cuda_cflags
 
 
 # Load the CUDA kernel as a python module
@@ -357,14 +281,6 @@ def get_mnk(sep: int = args.SEP):
     Ns = list(range(sep, args.MMNK + sep, sep))
     Ks = list(range(sep, args.MMNK + sep, sep))
     return Ms, Ns, Ks
-
-
-@torch.no_grad
-def row2col(x: torch.Tensor):
-    # convert a row major tensor -> col major with contiguous storage
-    x_trans = x.t()
-    x_col_major = x_trans.reshape(x.shape)
-    return x_col_major.contiguous() # must be a contiguous tensor
 
 
 Ms, Ns, Ks = get_mnk()
