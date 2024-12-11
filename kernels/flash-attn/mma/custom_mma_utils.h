dd966c0ff93b27e0833584ede557bb953b243cbb
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-12-11 17:19:19 +0800

    Update custom_mma_utils.h

--- kernels/flash-attn/mma/custom_mma_utils.h
+++ kernels/flash-attn/mma/custom_mma_utils.h
@@ -63,7 +63,7 @@ template<typename T, const int kWarpSize = WARP_SIZE>
 DEVICE_INLINE T warp_reduce_sum(T val) {
   #pragma unroll
   for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
-    val += __shfl_xor_sync(0xffffffff, val, mask);
+    val += __shfl_xor_sync(0xffffffff, val, mask, kWarpSize);
   }
   return val;
 }
@@ -72,7 +72,7 @@ template<typename T, const int kWarpSize = WARP_SIZE>
 DEVICE_INLINE T warp_reduce_max(T val) {
   #pragma unroll
   for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
-    T val_compare = __shfl_xor_sync(0xffffffff, val, mask);
+    T val_compare = __shfl_xor_sync(0xffffffff, val, mask, kWarpSize);
     val = val > val_compare ? val : val_compare;
   }
   return val;
