d5ceef6f8be9592bde26ac31c889fd7cc23fd15b
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-12-12 10:05:50 +0800

    Update flash_attn_mma.py

--- kernels/flash-attn/flash_attn_mma.py
+++ kernels/flash-attn/flash_attn_mma.py
@@ -10,38 +10,16 @@ import argparse
 import random
 import numpy as np
 
-
-torch.set_printoptions(
-    precision=6,        # 设置浮点数的小数位数
-    threshold=8,     # 当张量元素超过这个值时，会使用省略号表示
-    edgeitems=3,        # 控制每行显示的首尾元素数量
-    linewidth=120,      # 每行的最大宽度
-    sci_mode=False  # 禁用科学计数法
-)
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--rand-q", '--rq', action="store_true")
-    parser.add_argument("--rand-k", '--rk', action="store_true")
-    parser.add_argument("--rand-v", '--rv', action="store_true")
-    parser.add_argument("--rand-qkv", '--rqkv', action="store_true")
-    parser.add_argument("--range-k", '--gk', action="store_true")
-    parser.add_argument("--naive", action="store_true")
-    parser.add_argument("--sdpa", action="store_true")
-    parser.add_argument("--check", '--ch', action="store_true")
-    parser.add_argument("--B", type=int, default=None)
-    parser.add_argument("--H", type=int, default=None)
-    parser.add_argument("--N", type=int, default=None)
-    parser.add_argument("--D", type=int, default=None)
-    parser.add_argument("--seed", type=int, default=None)
-    parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--warmup", type=int, default=2)
-    parser.add_argument("--iters", type=int, default=10)
-    return parser.parse_args()
+torch.set_grad_enabled(False)
+torch.set_printoptions(precision=6, threshold=8, edgeitems=3, 
+                       linewidth=120, sci_mode=False)
 
 
-args = get_args()
-print(args)
+def set_rand_seed(seed:int=1):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
 
 def get_project_dir():
@@ -52,14 +30,32 @@ def get_project_dir():
 project_dir = get_project_dir()
 
 
-def set_rand_seed(seed:int=1):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--no-rand-q", '--no-rq', action="store_true")
+    parser.add_argument("--no-rand-k", '--no-rk', action="store_true")
+    parser.add_argument("--no-rand-v", '--no-rv', action="store_true")
+    parser.add_argument("--no-rand-qkv", '--no-rqkv', action="store_true")
+    parser.add_argument("--naive", action="store_true")
+    parser.add_argument("--sdpa", action="store_true")
+    parser.add_argument("--check", action="store_true")
+    parser.add_argument("--show-all", '--show', action="store_true")
+    parser.add_argument("--B", type=int, default=None)
+    parser.add_argument("--H", type=int, default=None)
+    parser.add_argument("--N", type=int, default=None)
+    parser.add_argument("--D", type=int, default=None)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--warmup", type=int, default=2)
+    parser.add_argument("--iters", type=int, default=10)
+    parser.add_argument("--range-k", '--gk', action="store_true")
+    return parser.parse_args()
 
 
-torch.set_grad_enabled(False)
+args = get_args()
+print(args)
+
+
 # Load the CUDA kernel as a python module
 lib = load(name='flash_attn_lib', 
            sources=[
@@ -82,25 +78,17 @@ lib = load(name='flash_attn_lib',
            extra_cflags=['-std=c++17'])
 
 
-# un-fused naive attn
-def naive_attn(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
-    att = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))
-    att = F.softmax(att, dim=-1)
-    y = att @ v
-    return y
-
-
 def run_benchmark(perf_func: callable, 
                   q: torch.Tensor, 
                   k: torch.Tensor, 
                   v: torch.Tensor,
                   tag: str, 
                   out: Optional[torch.Tensor] = None, 
-                  s: Optional[torch.Tensor] = None, 
+                  s: Optional[torch.Tensor] = None, # BUDEG
                   stages: int = -1,
                   warmup: int = args.warmup, 
                   iters: int = args.iters,
-                  show_all: bool = False):
+                  show_all: bool = args.show_all):
     if out is not None: 
         out.fill_(0)
     if s is not None:
@@ -148,13 +136,45 @@ def run_benchmark(perf_func: callable,
     print(f"{out_info:>20}: {out_val}, time:{mean_time:.6f}ms")
     if show_all: 
         print(out)
+    time.sleep(0.05)
     return out.clone(), mean_time
 
 
-Bs = [1] if not args.B else [args.B]
-Hs = [1] if not args.H else [args.H]
-Ns = [64] if not args.N else [args.N]
-Ds = [64] if not args.D else [args.D] 
+def get_qkvo(B, H, N, D):
+    if not (args.no_rand_q or args.no_rand_qkv):
+        q = torch.randn((B, H, N, D), dtype=torch.half, device="cuda")
+    else:
+        q = torch.ones(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
+    if not (args.no_rand_k or args.no_rand_qkv):
+        k = torch.randn((B, H, N, D), dtype=torch.half, device="cuda")
+    else:
+        k = torch.ones(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
+        if args.range_k:
+            for i in range(N):
+                k[:, :, i, :] = (i + 1) / N
+            k = k.cuda().half().contiguous()
+    if not (args.no_rand_v or args.no_rand_qkv):
+        v = torch.randn((B, H, N, D), dtype=torch.half, device="cuda")
+    else:
+        v = torch.ones(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
+
+    o = torch.zeros(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
+
+    return q, k, v, o
+
+
+# un-fused naive attn
+def naive_attn(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+    att = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))
+    att = F.softmax(att, dim=-1)
+    y = att @ v
+    return y
+
+
+Bs = [1, 2, 4] if not args.B else [args.B]
+Hs = [1, 4, 8] if not args.H else [args.H]
+Ns = [1024, 2048] if not args.N else [args.N]
+Ds = [64, 128] if not args.D else [args.D] 
 # batch_size, n_head, seq_len, head_dim (B,H,N,D)
 BHNDs = [(B, H, N, D) for B in Bs for H in Hs for N in Ns for D in Ds]
 
@@ -163,54 +183,38 @@ set_rand_seed(seed)
 print("-" * 100)
 print(" "* 10 + f"B: batch_size, H: n_head, N: seq_len, D: head_dim, "
       f"seed: {seed}, Warmup: {args.warmup}, Iters: {args.iters}")
+
 for (B, H, N, D) in BHNDs:
     print("-" * 100)
-    print(" " * 40 + f"B={B}, H={H}, N={N}, D={D}")
-    if args.rand_q or args.rand_qkv:
-        q = torch.randn((B, H, N, D), dtype=torch.half, device="cuda")
-    else:
-        q = torch.ones(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
-    if args.rand_k or args.rand_qkv:
-        k = torch.randn((B, H, N, D), dtype=torch.half, device="cuda")
-    else:
-        k = torch.ones(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
-        if args.range_k:
-            for i in range(N):
-                k[:, :, i, :] = (i + 1) / N
-            k = k.cuda().half().contiguous()
-    if args.rand_v or args.rand_qkv:
-        v = torch.randn((B, H, N, D), dtype=torch.half, device="cuda")
-    else:
-        v = torch.ones(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
-    o = torch.zeros(B, H, N, D, device="cuda", dtype=torch.half).contiguous()
+    print(" " * 25 + f"B={B}, H={H}, N={N}, D={D}, Warmup: {args.warmup}, Iters: {args.iters}")
+    q, k, v, o = get_qkvo(B, H, N, D)
     tk = k.transpose(-2, -1).contiguous()
-    fq = q.transpose(1, 2)
-    fk = k.transpose(1, 2)
-    fv = v.transpose(1, 2)
+    fq = q.transpose(1,   2).contiguous()
+    fk = k.transpose(1,   2).contiguous()
+    fv = v.transpose(1,   2).contiguous()
     torch.cuda.synchronize()
     
     if args.naive:
-        out_naive, _ = run_benchmark(naive_attn, q, k, v, "(naive)")
+        out_naive,  _ = run_benchmark(naive_attn, q, k, v, "naive(unfused)")
+
     # using fp16 Tesor Core MMA instruction
-    out_mma_naive, _ = run_benchmark(lib.flash_attn_mma_naive, q, k, v, "mma(naive)", o)
+    out_mma_naive,  _ = run_benchmark(lib.flash_attn_mma_naive, q, k, v, "mma(naive)", o)
     out_mma_stage1, _ = run_benchmark(lib.flash_attn_mma_stages, q, tk, v, "mma(stage1)", o, stages=1)
     out_mma_stage2, _ = run_benchmark(lib.flash_attn_mma_stages, q, tk, v, "mma(stage2)", o, stages=2)
-    out_flash, _ = run_benchmark(flash_attn_func, fq, fk, fv, "(flash)")
+    out_flash,      _ = run_benchmark(flash_attn_func, fq, fk, fv, "(flash)")
+
     if args.sdpa:
-        out_sdpa, _ = run_benchmark(F.scaled_dot_product_attention, q, k, v, "(sdpa)")
+        out_sdpa,   _ = run_benchmark(F.scaled_dot_product_attention, q, k, v, "(sdpa)")
     
+    torch.cuda.synchronize()
     if args.check:
+        out_flash = out_flash.transpose(1, 2)
+        for i in range(int(N/8)):
+            if i < 4:
+                print("-" * 100)
+                print(f"out_flash[:, :,  {(i*8)}:{(i+1)*8}, :]:\n")
+                print(out_flash[:, :,  (i*8):(i+1)*8, :].float())
+                print(f"out_mma_stage1[:, :, {(i*8)}:{(i+1)*8}, :]:\n")
+                print(out_mma_stage1[:, :, (i*8):(i+1)*8, :].float())
         print("-" * 100)
-        print(out_mma_naive[:, :, :8, :].float())
-        print(out_mma_stage1[:, :, :8, :].float())
-        print("-" * 100)
-        print(out_mma_naive[:, :, 8:16, :].float())
-        print(out_mma_stage1[:, :, 8:16, :].float())
-        print("-" * 100)
-        print(out_mma_naive[:, :, 16:24, :].float())
-        print(out_mma_stage1[:, :, 16:24, :].float())
-        print("-" * 100)
-        print(out_mma_naive[:, :, 24:32, :].float())
-        print(out_mma_stage1[:, :, 24:32, :].float())
-        print("-" * 100)
-        print(f"{torch.allclose(out_mma_stage1.float(), out_mma_naive.float(), atol=1e-2)}")
+        print(f"{torch.allclose(out_flash.float(), out_mma_naive.float(), atol=1e-2)}")
