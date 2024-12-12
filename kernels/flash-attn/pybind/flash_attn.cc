f00af00d1b27dac4941fd587ed8f90fb44d0f837
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-12-12 17:43:11 +0800

    Update flash_attn.cc

--- kernels/flash-attn/pybind/flash_attn.cc
+++ kernels/flash-attn/pybind/flash_attn.cc
@@ -8,11 +8,11 @@
 void flash_attn_cuda(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O);
 void flash_attn_mma_naive(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O);
 void flash_attn_mma_stages(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O, int stages);
-void flexiable_flash_attn_mma_stages(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O, int stages);
+void flexiable_flash_attn_mma_stages_split_kv(torch::Tensor Q, torch::Tensor K, torch::Tensor V, torch::Tensor O, int stages);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   TORCH_BINDING_COMMON_EXTENSION(flash_attn_cuda)
   TORCH_BINDING_COMMON_EXTENSION(flash_attn_mma_naive)
   TORCH_BINDING_COMMON_EXTENSION(flash_attn_mma_stages)
-  TORCH_BINDING_COMMON_EXTENSION(flexiable_flash_attn_mma_stages)
+  TORCH_BINDING_COMMON_EXTENSION(flexiable_flash_attn_mma_stages_split_kv)
 }
