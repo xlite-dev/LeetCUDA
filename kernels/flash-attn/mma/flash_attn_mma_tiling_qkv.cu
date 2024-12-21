92254d10e28a5111f8eb18b7ba05c56b11ca51af
DefTruth <qiustudent_r@163.com>
2024-12-21 21:11:05 +0800

    support flash-attn-mma tiling-qk

--- kernels/flash-attn/mma/flash_attn_mma_tiling_qkv.cu
+++ kernels/flash-attn/mma/flash_attn_mma_tiling_qkv.cu
@@ -3,3 +3,4 @@
 // NOTE: For R_V[kWarpTileHeadDimV][2], kWarpTileHeadDimV will increase with d.
 // so, for large d, R_V will need more registers and cause performance down.
 // We have to find a way to apply MMA level tiling for V(R_V) for large d.
+// Also, R_O and R_D will bound by registers resources.
