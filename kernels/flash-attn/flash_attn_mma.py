f3c1b5af58034299d51356eb5ea2130b3b809010
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-12-26 18:41:05 +0800

    Update flash_attn_mma.py

--- kernels/flash-attn/flash_attn_mma.py
+++ kernels/flash-attn/flash_attn_mma.py
@@ -18,28 +18,6 @@ torch.set_printoptions(precision=6, threshold=8, edgeitems=3,
                        linewidth=120, sci_mode=False)
 
 
-def set_rand_seed(seed:int=1):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-
-def get_project_dir():
-    return os.path.dirname(os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))))
-
-
-def pretty_print_line(m: str = "", sep: str = "-", width: int = 150):
-    res_len = width - len(m)
-    left_len = int(res_len / 2)
-    right_len = res_len - left_len
-    pretty_line = sep * left_len + m + sep * right_len
-    print(pretty_line)
-
-
-project_dir = get_project_dir()
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -69,39 +47,121 @@ def get_args():
 
 
 args = get_args()
+
+
+def set_rand_seed(seed:int=1):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def get_device_name():
+    device_name = torch.cuda.get_device_name(torch.cuda.current_device())
+    # since we will run GPU on WSL2, so add WSL2 tag.
+    if "Laptop" in device_name:
+        device_name += " WSL2"
+    return device_name
+
+
+def get_device_capability():
+    return torch.cuda.get_device_capability(torch.cuda.current_device())
+
+
+def get_build_sources():
+    build_sources = []
+    build_sources.append('./mma/flash_attn_mma_split_kv.cu')
+    build_sources.append('./mma/flash_attn_mma_split_q.cu')
+    build_sources.append('./mma/flash_attn_mma_share_kv.cu')
+    build_sources.append('./mma/flash_attn_mma_share_qkv.cu')
+    build_sources.append('./mma/flash_attn_mma_tiling_qk.cu')
+    build_sources.append('./mma/flash_attn_mma_tiling_qk_swizzle.cu')
+    build_sources.append('./mma/flash_attn_mma_share_kv_swizzle.cu')
+    build_sources.append('./mma/flash_attn_mma_share_kv_fully_swizzle.cu')
+    build_sources.append('./pybind/flash_attn.cc')
+    return build_sources
+
+
+def get_project_dir():
+    return os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))))
+
+
+project_dir = get_project_dir()
+
+
+def get_build_cuda_cflags(build_pkg: bool = False):
+    # -Xptxas -v:
+    # registers, smem, cmem, stack, gmem usage
+    # registers: 寄存器，访问速度最快。Ada Lovelace架构每个SM的寄存器文件大小
+    # 为256KB，这相当于65536个32位寄存器，65536/256=256。一个SM可以同时执行多
+    # 个block，对一个Kernel，同时存在于一个SM中的Block和Warp数量取决于SM中可用
+    # 且所需的寄存器和共享内存数量。每个Thread需要的寄存器越多，那么SM中的Warp就
+    # 越少。即减少Thread所需寄存器数量，即可增加SM中的Warp数。每个Block需要的共
+    # 享内存越多，那么SM中可以被同时处理的Block就会变少。即减少每个Block所需的共
+    # 享内存，即可同时处理更多Block。SM内的资源没办法处理一个完整Block，Kernel
+    # 将无法启动。
+    # cmem: 常量内存，被缓存，访问速度快。
+    # stack frame: 由于寄存器的数量有限，当需要使用的变量数量超过可用寄存器数量时，
+    # 编译器会将某些变量从寄存器“溢出”到栈上，这个过程称为spill。访问栈上的数据比
+    # 访问寄存器慢得多。
+    # spill stores: 指的是在执行过程中，数据因为寄存器不足而被存储到了栈上。
+    # spill loads: 则是指将之前溢出到栈上的数据重新加载回寄存器。
+    # diag 177: variable was declared but never referenced
+    extra_cuda_cflags = []
+    extra_cuda_cflags.append("-O3")
+    extra_cuda_cflags.append("-std=c++17")
+    extra_cuda_cflags.append("-U__CUDA_NO_HALF_OPERATORS__")
+    extra_cuda_cflags.append("-U__CUDA_NO_HALF_CONVERSIONS__")
+    extra_cuda_cflags.append("-U__CUDA_NO_HALF2_OPERATORS__")
+    extra_cuda_cflags.append("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
+    extra_cuda_cflags.append("--expt-relaxed-constexpr")
+    extra_cuda_cflags.append("--expt-extended-lambda")
+    extra_cuda_cflags.append("--use_fast_math")
+    extra_cuda_cflags.append("-DFLASH_ATTN_MMA_DEBUG" if args.debug else "")
+    device_name = get_device_name()
+    if "L20" in device_name:
+        extra_cuda_cflags.append("-DBUILD_FLASH_ATTN_MMA_L20")
+    if "4090" in device_name:
+        extra_cuda_cflags.append("-DBUILD_FLASH_ATTN_MMA_4090")
+    if not build_pkg:
+      extra_cuda_cflags.append("-diag-suppress 177")
+      extra_cuda_cflags.append("-Xptxas -v")
+    else:
+      extra_cuda_cflags.append("--ptxas-options=-v")
+      extra_cuda_cflags.append("--ptxas-options=-O3")
+    # extra cuda flags for cute hgemm
+    project_dir = get_project_dir()
+    # add cutlass headers and link cublas.
+    extra_cuda_cflags.append(f'-I {project_dir}/kernels/flash-attn')
+    extra_cuda_cflags.append(f'-I {project_dir}/kernels/flash-attn/utils')
+    extra_cuda_cflags.append(f'-I {project_dir}/kernels/flash-attn/mma')
+    extra_cuda_cflags.append(f'-I {project_dir}/kernels/flash-attn/cutlass')
+    extra_cuda_cflags.append(f'-I {project_dir}/kernels/flash-attn/pybind')
+    extra_cuda_cflags.append(f'-I {project_dir}/third-party/cutlass/include')
+    extra_cuda_cflags.append(f'-I {project_dir}/third-party/cutlass/tools/util/include')
+    return extra_cuda_cflags
+
+def pretty_print_line(m: str = "", sep: str = "-", width: int = 150):
+    res_len = width - len(m)
+    left_len = int(res_len / 2)
+    right_len = res_len - left_len
+    pretty_line = sep * left_len + m + sep * right_len
+    print(pretty_line)
+
+
+
 if args.D and args.D >= 256:
     args.run_torch_sdpa = True
 pretty_print_line()
 print(args)
 pretty_print_line()
 
+
 # Load the CUDA kernel as a python module
 lib = load(name='flash_attn_lib', 
-           sources=[
-               './mma/flash_attn_mma_split_kv.cu',
-               './mma/flash_attn_mma_split_q.cu',
-               './mma/flash_attn_mma_share_kv.cu',
-               './mma/flash_attn_mma_share_qkv.cu',
-               './mma/flash_attn_mma_tiling_qk.cu',
-               './mma/flash_attn_mma_tiling_qk_swizzle.cu',
-               './mma/flash_attn_mma_share_kv_swizzle.cu',
-               './mma/flash_attn_mma_share_kv_fully_swizzle.cu',
-               './pybind/flash_attn.cc'
-            ], 
-           extra_cuda_cflags=[
-               "-O3",
-                "-U__CUDA_NO_HALF_OPERATORS__",
-                "-U__CUDA_NO_HALF_CONVERSIONS__",
-                "-U__CUDA_NO_HALF2_OPERATORS__",
-                "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
-                "--expt-relaxed-constexpr",
-                "--expt-extended-lambda",
-                "--use_fast_math",
-                "-Xptxas -v",
-                "-diag-suppress 177",
-                f"-I {project_dir}/kernels/flash-attn/utils",
-                "-DFLASH_ATTN_MMA_DEBUG" if args.debug else ""
-            ], 
+           sources=get_build_sources(), 
+           extra_cuda_cflags=get_build_cuda_cflags(), 
            extra_cflags=['-std=c++17'],
            verbose=args.verbose)
 
